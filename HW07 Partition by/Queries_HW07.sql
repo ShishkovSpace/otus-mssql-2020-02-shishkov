63d734f92ca10164f5b443d3cc33e603b49a4716
Aleksandr Shishkov <Shishkv07@yandex.ru>
2020-04-04 02:58:51 +0300

    Added tasks 3-6 for HW07

--- HW07 Partition by/Queries_HW07.sql
+++ HW07 Partition by/Queries_HW07.sql
@@ -72,5 +72,84 @@ where year(i.InvoiceDate)>=2015
 order by year(i.InvoiceDate), month(i.InvoiceDate);
 
 /*
-* 3. One-query functions
+* 3. Top 2 most popular StockItemID per month for 2016
 */
+select tt.ID, tt.Year, tt.Month, tt.IDAmount
+from (
+	select	t.*,
+			ROW_NUMBER() over (partition by t.Year, t.Month order by t.IDAmount desc) as Rank
+	from (
+		select	distinct ol.StockItemID as ID,
+				year(o.OrderDate) as Year,
+				month(o.OrderDate) as Month,
+				count(ol.StockItemID) over (partition by year(o.OrderDate), month(o.OrderDate), ol.StockItemID) as IDAmount
+		from Sales.Orders o
+		inner join Sales.OrderLines ol on o.OrderID=ol.OrderID
+		where year(o.OrderDate)=2016
+	) as t
+) as tt
+where tt.Rank <=2;
+
+/*
+* 4. One-query function
+*/
+select	StockItemID,
+		StockItemName,
+		Brand,
+		UnitPrice,
+		ROW_NUMBER() over (partition by SUBSTRING(StockItemName,1,1) order by StockItemID) as AlphabetSort,
+		COUNT(StockItemID) over () as TotalCount,
+		COUNT(StockItemID) over (partition by SUBSTRING(StockItemName,1,1)) as AlphabetCount,
+		LEAD(StockItemID) over (partition by SUBSTRING(StockItemName,1,1) order by StockItemName) as NextID,
+		LAG(StockItemID) over (partition by SUBSTRING(StockItemName,1,1) order by StockItemName) as PreviousID,
+		LAG(StockItemName,2,'No items') over (order by StockItemName) as TwoRowsPreviousName,
+		NTILE(30) over (partition by TypicalWeightPerUnit order by StockItemID) as Groups
+from Warehouse.StockItems
+order by Groups;
+
+/*
+* 5. Last client for each employee
+*/
+select	distinct tt.Employee,
+		p.FullName,
+		tt.Customer,
+		c.CustomerName,
+		tt.EmployeeDates,
+		SUM(il.Quantity*il.UnitPrice) over (partition by tt.ID)
+from (
+	select	t.*,
+			ROW_NUMBER() over (partition by t.Employee order by t.ID desc) as SortValues
+	from (
+		select	i.InvoiceID as ID, i.SalesPersonPersonID as Employee, i.CustomerID as Customer, 
+				max(i.InvoiceDate) over (partition by i.SalesPersonPersonID order by i.InvoiceDate desc) as EmployeeDates
+		from Sales.Invoices i
+	) as t	
+) as tt
+inner join Sales.InvoiceLines il on tt.ID=il.InvoiceID
+inner join Sales.Customers c on c.CustomerID=tt.Customer
+inner join Application.People p on p.PersonID=tt.Employee
+where tt.SortValues=1;
+
+/*
+* 6. Top 2 most expensive StockItemID
+*/
+select Result.CustomerID, Result.CustomerName, Result.StockItemID, Result.ItemPrice, Result.InvoiceDate
+from (
+	select	tt.*,
+			ROW_NUMBER() over (partition by tt.CustomerID, tt.StockItemID order by tt.InvoiceDate) as SortItems,
+			DENSE_RANK() over (partition by tt.CustomerID order by tt.StockItemID desc) as RankedItem
+	from (
+		select	t.*,
+				DENSE_RANK() over (partition by t.CustomerID order by t.ItemPrice desc) as RankedPrice
+		from (
+			select	c.CustomerID, c.CustomerName, il.StockItemID, 
+					MAX(il.UnitPrice) over (partition by i.CustomerID, il.StockItemID) as ItemPrice,
+					i.InvoiceDate
+			from Sales.Customers c
+			inner join Sales.Invoices i on i.CustomerID=c.CustomerID
+			inner join Sales.InvoiceLines il on il.InvoiceID=i.InvoiceID
+		) as t
+	) as tt
+	where tt.RankedPrice<=2
+) as Result
+where Result.SortItems=1 and Result.RankedItem<=2;
