1349852bdfcfbe9c5c7f495ac8478196107c0d27
Alexander Shishkov <Shishkv07@yandex.ru>
2020-05-17 16:56:31 +0300

    Add HW11 CLR with using SQL#

---
+++ HW11 CLR/Queries_HW11.sql
@@ -0,0 +1,11 @@
+GO
+SELECT SQL#.Math_CubeRoot(27);
+
+GO
+SELECT SQL#.String_Contains('TestString', 'Str');
+
+GO
+SELECT SQL#.String_TryParseToInt('123,456', 'int', 'en-US');
+
+GO
+SELECT SQL#.Math_RoundToEvenFloat(7.5506091, 0);
